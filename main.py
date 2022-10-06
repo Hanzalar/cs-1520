ee0264cf3f0b36ac62c5fe96ad19a5af48c4dc58
David Happe <djh111@pitt.edu>
2022-10-06 18:04:09 -0400

    initial flask templating

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask import Response
+from flask import render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route(/index.html')
 def root():
-    return Response('For now, check out /static/default.html', mimetype='text/plain')
+    return render_template('index.html', title='Home')
+
+@app.errorhandler(404)
+@app.route('/404.html')
+@app.route('/404')
+def error404(e):
+    return render_template('404.html', title='404'), 404
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
