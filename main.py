eb5598a1a979bbc57ea5a110d5f83c905dc3e169
Derrick Hicks <ddh32@pitt.edu>
2022-10-08 16:01:27 +0000

    questionnaire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import Response
+from flask import request
 from flask import render_template
 from random import shuffle
 
@@ -18,6 +19,27 @@ def root():
      ] # this assumes that for name there is an image file at /static/images/name.png
     shuffle(founder_list) # random order of pics bc why not
     return render_template('index.html', title='Home', names=founder_list)
+
+@app.route('/roomieQuiz')
+@app.route('/roomieQuiz.html')
+def questionnaire():
+    return render_template('roomieQuiz.html', title='Questionnaire')
+
+@app.route('/DisplayQuestionnaire')
+def DisplayQuestionnaire():
+    try:
+        name = request.args["name"]
+        prsntype ="Personality Type: %s" % (request.args["type"])
+        pet = "Pet Policy: %s" % (request.args["pet"])
+        smoke = "Smoke Policy: %s" % (request.args["smoke"])
+        friends = "Friend Policy: %s" % (request.args["friends"])
+        bedtime = "Bedtime: %s" % (request.args["bedtime"])
+
+        list = [prsntype, pet, smoke, friends, bedtime]
+
+        return render_template('quizResults.html', title = "Results", Name = name, results = list)
+    except:
+        return questionnaire()
 
 @app.errorhandler(404)
 @app.route('/404.html')
