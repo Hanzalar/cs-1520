d85c457d8bbf99f4d1b712aa7c04e13ac8e07b2c
Derrick Hicks <ddh32@pitt.edu>
2022-11-07 00:30:15 +0000

    singup and login

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from flask import Flask
 from flask import Response
 from flask import request
 from flask import render_template
+from flask import flash
 from random import shuffle
+from werkzeug.security import generate_password_hash, check_password_hash
+from google.cloud import datastore
 
 
 app = Flask(__name__)
+
+def create_user():
+    client = datastore.Client()
+    key = client.key('user')
+    return datastore.Entity(key)
 
 @app.route('/')
 @app.route('/index.html')
@@ -22,6 +30,50 @@ def root():
     if (founder_list[0] == 'David'): alert_msg = "This is an error!"
     return render_template('index.html', title='Home', names=founder_list, alert_msg=alert_msg)
 
+
+@app.route('/signup', methods=['POST','GET'])
+def signup():
+    email = request.values['email']
+    name = request.values['name']
+    password = request.values['password']
+
+    user_exist = datastore.Client().query(kind = 'user')
+    user_exist.add_filter('email','=', email)
+    
+    for user in user_exist.fetch():
+        if user['email'] == email:
+            return render_template('login.html')
+
+    new_user = create_user()
+    new_user['name'] = name
+    new_user['email'] = email
+    new_user['password'] = generate_password_hash(password, method='sha256')
+
+    datastore.Client().put(new_user)
+
+    return render_template('profile.html')
+
+@app.route('/login')
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/loginpost', methods=['POST','GET'])
+def handle_request_login():
+    email = request.values['email']
+    password = request.values['password']
+
+    user_exist = datastore.Client().query(kind='user')
+    user_exist.add_filter('email','=',email)
+
+    user = list(user_exist.fetch())[0]
+
+    if not user or not check_password_hash(user['password'], password):
+        return render_template('login.html')
+    
+    return render_template('profile.html')
+
+
 @app.route('/roomieQuiz')
 @app.route('/roomieQuiz.html')
 def questionnaire():
@@ -30,12 +82,12 @@ def questionnaire():
 @app.route('/DisplayQuestionnaire')
 def DisplayQuestionnaire():
     try:
-        name = request.args["name"]
-        prsntype ="Personality Type: %s" % (request.args["type"])
-        pet = "Pet Policy: %s" % (request.args["pet"])
-        smoke = "Smoke Policy: %s" % (request.args["smoke"])
-        friends = "Friend Policy: %s" % (request.args["friends"])
-        bedtime = "Bedtime: %s" % (request.args["bedtime"])
+        name = request.values["name"]
+        prsntype ="Personality Type: %s" % (request.values["type"])
+        pet = "Pet Policy: %s" % (request.values["pet"])
+        smoke = "Smoke Policy: %s" % (request.values["smoke"])
+        friends = "Friend Policy: %s" % (request.values["friends"])
+        bedtime = "Bedtime: %s" % (request.values["bedtime"])
 
         list = [prsntype, pet, smoke, friends, bedtime]
 
@@ -50,8 +102,8 @@ def Join():
 @app.route('/emailsubmission')
 def emailsubmission():
      try:
-         name = request.args["name"]
-         email = request.args["email"]
+         name = request.values["name"]
+         email = request.values["email"]
          return render_template('JoinSuccess.html', title = "Success", Name = name, results = email)
      except:
         return Join()
