2311f3db8855eade90295777405041a859618dd8
Hanzalar <hanzala@gmail.com>
2022-11-09 21:38:05 -0500

    frontend fixes to login/sigup and hyperlinking fixes

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask import request
 from flask import render_template
 from flask import flash
 from flask import session
-from flask_login import current_user, UserMixin, LoginManager, login_required, login_user, logout_user
 from random import shuffle
 from werkzeug.security import generate_password_hash, check_password_hash
 from google.cloud import datastore
